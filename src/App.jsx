6043d36a7d68dd1e85bbf0e693aded5e5501ada2
nessipenko <101459146+nessipenko@users.noreply.github.com>
2023-02-21 19:27:12 +0300

    add Header & Container

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Container } from './components/Container/Container';
+import { Header } from './components/Header/Header';
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <nav>
+          <Container
+            maks="leskin"
+            className="navigation__container"
+          />
+        </nav>
+        <section></section>
+      </main>
+      <footer></footer>
+    </>
+  );
+};
