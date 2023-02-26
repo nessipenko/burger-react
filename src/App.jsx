6b2c1f738a09f437483981de20efc3b281d519ce
nessipenko <101459146+nessipenko@users.noreply.github.com>
2023-02-26 13:24:12 +0300

    add CategorySlice

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
+import { Provider } from 'react-redux';
 import { Catalog } from './components/Catalog/Catalog';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { Navigation } from './components/Navigation/Navigation';
+import { store } from './store/index.js';
 
 export const App = () => {
-  return (
-    <>
-      <Header />
-      <main>
-        <Navigation />
-        <Catalog />
-      </main>
-      <Footer />
-    </>
-  );
+    return (
+        <Provider store={store}>
+            <Header />
+            <main>
+                <Navigation />
+                <Catalog />
+            </main>
+            <Footer />
+        </Provider>
+    );
 };
---
+++ src/components/Count/Count.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import style from './Count.module.css'
+
+export const Count = (props) => {
+    const [count, setCount] = useState(props.count)
+
+    const addCount = () => {
+        setCount(count + 1)
+    }
+
+    const removeCount = () => {
+        if (count > 1) {
+            setCount(count - 1)
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+    return (
+        <div className={style.count}>
+            <button onClick={removeCount} className={style.minus} disabled={count === 1}>-</button>
+            <p className={style.amount}>{count}</p>
+            <button onClick={addCount} className={style.plus}>+</button>
+        </div>
+    )
+}
